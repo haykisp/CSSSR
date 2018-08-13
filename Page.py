62c242a89ffffc05cccfc6f51110fc591aa1ff1d
hayk <hayk.ispiryan@betconstruct.com>
2018-08-13 16:48:25 +0400

    added comments

--- Page.py
+++ Page.py
@@ -1,27 +1,36 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
 
 class Page():
     def __init__(self):
+        """
+        Constructor of web page.
+        Opens browser with our web page
+        """
         self.URL = 'http://blog.csssr.ru/qa-engineer/'
         self.driver = webdriver.Chrome(executable_path='chromedriver')
         self.driver.maximize_window()
         self.driver.get(self.URL)
 
-    def click(self,XPath):
-        self.driver.find_element_by_xpath(XPath).click()
+    def click(self, element):
+        """
+        This method clicks on element, which given as argument
+        :param element:
+        :return:
+        """
+        element.click()
 
-    def wait(self,XPath,time):
-        wait = WebDriverWait(self.driver, time)
-        wait.until(EC.element_to_be_clickable((By.XPATH, XPath)))
 
     def content(self):
+        """
+        Ceating element
+        :return:
+        """
         xpath = '//div[@class="wrap"]'
         return self.driver.find_element_by_xpath(xpath)
 
     def close(self):
+        """
+        This method closing browser
+        :return:
+        """
         self.driver.quit()
-
--- TestCases.py
+++ TestCases.py
@@ -1,5 +1,4 @@
 import unittest
-
 from Page import Page
 
 
@@ -15,11 +14,17 @@ class testCases(unittest.TestCase):
 
 
     def test1(self):
+        """
+        checking width of content with expected value from TS
+        """
         width = self.page.content().size['width']
         self.assertEqual(1000, width)
 
     def test2(self):
+        """
+        checking content position with expected position
+        """
         margin_left = self.page.content().value_of_css_property("margin-left")
-        self.assertEqual(24, margin_left)
+        self.assertEqual('24px', margin_left)
 
 
